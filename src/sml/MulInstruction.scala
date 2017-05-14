88b3b1acb345c99f9f932f47378d535b00da4ce0
julesss1 <heissl@hotmail.com>
2017-05-14 22:14:00 +0100

    Update MulInstruction.scala

--- src/sml/MulInstruction.scala
+++ src/sml/MulInstruction.scala
@@ -4,8 +4,11 @@ class MulInstruction(label: String, op: String, val result: Int, val op1: Int, v
   extends Instruction(label, op) {
 
   override def execute(m: Machine) {
+    //get first value of register
     val value1 = m.regs(op1)
+    //get value of second register
     val value2 = m.regs(op2)
+    //multiply and store result in third register
     m.regs(result) = value1 * value2
   }
 
@@ -14,6 +17,7 @@ class MulInstruction(label: String, op: String, val result: Int, val op1: Int, v
   }
 }
 
+//companion object to create object
 object MulInstruction {
   def apply(label: String, result: Int, op1: Int, op2: Int) =
     new MulInstruction(label, "mul", result, op1, op2)
