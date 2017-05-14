4f6963ad942b6e09f9b318921bd2bf21b64cbe57
julesss1 <heissl@hotmail.com>
2017-05-14 22:15:44 +0100

    Update SubInstruction.scala

--- src/sml/SubInstruction.scala
+++ src/sml/SubInstruction.scala
@@ -4,8 +4,11 @@ class SubInstruction(label: String, op: String, val result: Int, val op1: Int, v
   extends Instruction(label, op) {
 
   override def execute(m: Machine) {
+    //get value of first register
     val value1 = m.regs(op1)
+    //get value of second register
     val value2 = m.regs(op2)
+    //subtract second value from first and store into third
     m.regs(result) = value1 - value2
   }
 
@@ -14,6 +17,7 @@ class SubInstruction(label: String, op: String, val result: Int, val op1: Int, v
   }
 }
 
+//companion object to create instances
 object SubInstruction {
   def apply(label: String, result: Int, op1: Int, op2: Int) =
     new SubInstruction(label, "sub", result, op1, op2)
